6cf179a71cbc69bafc9a8ab87a2ece86a3b2b13b
Lucas Alegre <lucasnale@gmail.com>
2018-06-27 17:55:45 -0300

    V

--- l1.ml
+++ l1.ml
@@ -33,7 +33,7 @@ type value = Vnum of int
            | Vrclos of variable * variable * expr * env
            | Vnil                     (* Valores adicionados a partir daqui *)
            | Vcons of value * value
-           | Raise    (* Raise NÂO DEVIA SER VALOR, MAS NÃO SEI COMO FAZER *)
+           | VRaise
 and
      env = (variable * value) list
 and
@@ -51,7 +51,7 @@ let uvargen =
 
 (* AVALIADOR BIG STEP *)
 exception NoRuleApplies of string
-let rec _eval (contexto : env) (e : expr) = ( match e with
+let rec _eval (contexto : env) (e : expr) : value = ( match e with
 
     (* BS-NUM *)
     Num(n) -> Vnum(n)
@@ -73,9 +73,9 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
             | (Greater, Vnum n1, Vnum n2) -> Vbool(n1 > n2)
             | (Or, Vbool b1, Vbool b2) -> Vbool(b1 || b2)
             | (And, Vbool b1, Vbool b2) -> Vbool(b1 && b2)
-            | (Div, Vnum n1, Vnum n2) -> if n2 != 0 then Vnum(n1 / n2) else Raise
-            | (_, Raise, _) -> Raise
-            | (_, _, Raise) -> Raise
+            | (Div, Vnum n1, Vnum n2) -> if n2 != 0 then Vnum(n1 / n2) else VRaise
+            | (_, VRaise, _) -> VRaise
+            | (_, _, VRaise) -> VRaise
             | _ -> raise (NoRuleApplies "Binary Operation Error")
         )
     )
@@ -85,7 +85,7 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
             match e1' with
               (Vbool true) -> _eval contexto e2
             | (Vbool false) -> _eval contexto e3
-            | Raise -> Raise
+            | VRaise -> VRaise
             | _ -> raise (NoRuleApplies "If condition Error")
         )
     )
@@ -98,8 +98,8 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
             match(e1', e2') with
             | (Vclos(x, e, env'), v) -> _eval ((x,v)::env') e
             | (Vrclos(f, x, e, env'), v) -> _eval ((x,v)::(f,Vrclos(f,x,e,env'))::env') e
-            | (Raise,_) -> Raise
-            | (_,Raise) -> Raise
+            | (VRaise,_) -> VRaise
+            | (_,VRaise) -> VRaise
             | _ -> raise (NoRuleApplies "Application Error")
         )
     )
@@ -109,12 +109,12 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
     (* BS-LET *)
     | Let(x, t, e1, e2) -> let e1' = (_eval contexto e1) in (
                                match e1' with
-                               | Raise -> Raise
+                               | VRaise -> VRaise
                                | _ -> _eval ((x,e1')::contexto) e2
                             )
     | LetI(x, e1, e2) -> let e1' = (_eval contexto e1) in (
                                match e1' with
-                               | Raise -> Raise
+                               | VRaise -> VRaise
                                | _ -> _eval ((x,e1')::contexto) e2
                             )
     (* BS-LETREC *)
@@ -129,8 +129,8 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
         let e1' = _eval contexto e1 in
         let e2' = _eval contexto e2 in (
             match (e1', e2') with
-            | (Raise,_) -> Raise
-            | (_,Raise) -> Raise
+            | (VRaise,_) -> VRaise
+            | (_,VRaise) -> VRaise
             | _ -> Vcons(e1',e2')
         )
     )
@@ -140,7 +140,7 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
             match e' with
             | Vnil -> (Vbool true)
             | Vcons(v1,v2) -> (Vbool false)
-            | Raise -> Raise
+            | VRaise -> VRaise
             | _ -> raise (NoRuleApplies "Invalid list")
         )
     )
@@ -148,9 +148,9 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
     | Hd(e) -> (
         let e' = _eval contexto e in (
             match e' with
-            | Vcons(v1,v2) -> v1
-            | Vnil -> Raise
-            | Raise -> Raise
+            | Vcons(v1,_) -> v1
+            | Vnil -> VRaise
+            | VRaise -> VRaise
             | _ -> raise (NoRuleApplies "Invalid list")
         )
     )
@@ -158,25 +158,25 @@ let rec _eval (contexto : env) (e : expr) = ( match e with
     | Tl(e) -> (
         let e' = _eval contexto e in (
             match e' with
-            | Vcons(v1,v2) -> v2
-            | Vnil -> Raise
-            | Raise -> Raise
+            | Vcons(_,v2) -> v2
+            | Vnil -> VRaise
+            | VRaise -> VRaise
             | _ -> raise (NoRuleApplies "Invalid list")
         )
     )
     (* BS-RAISE *)
-    | Raise -> Raise
+    | Raise -> VRaise
     (* BS-TRYWITH *)
     | TryWith(e1,e2) -> (
         let e1' = _eval contexto e1 in (
             match e1' with
-            | Raise -> _eval contexto e2
+            | VRaise -> _eval contexto e2
             | _ -> e1'
         )
     )
 )
 
-let eval e = _eval [] e
+let eval (e: expr) : value = _eval [] e
 (**********************************************************)
 
 
@@ -199,7 +199,7 @@ let rec valueToString (tp:value) : string =
   | Vrclos(var1,var2,expr1,env) -> "rclos"
   | Vnil -> "vnil"
   | Vcons(val1, val2) -> "vcons"
-  | Raise -> "raise"
+  | VRaise -> "raise"
 ;;
 
 let valueToTipo (tp:value) : tipo =
@@ -531,8 +531,11 @@ let e13 = Tl(Cons(Num(5), (Cons (Num(5), Nil))))
 
 let e14 = TryWith(Raise, Bop(Sum,Num(5),Num(10)))
 
-let e15 = TryWith(Bop(Sum,Num(5),Num(10)), Bop(Sum,Num(5),Num(10)))
+let e15 = TryWith(Bop(Sum,Num(7),Num(10)), Bop(Sum,Num(5),Num(10)))
 
+let e16 = Raise
+
+let e17 = Cons(Bop(Sum, Num(7), Raise), Nil)
 
 (* Testes que NÂO devem retornar um tipo *)
 let ne1 = Bop(Sum,Num(5),Bool(true))
@@ -574,7 +577,7 @@ let ne14 = Tl(Cons(Num(5), (Cons (Bool(true), Nil))))
 let ne15 = TryWith(Bool(true), Bop(Sum,Num(5),Num(10)))
 
 
-let allEs = [e1;e2;e3;e4;e5;e6;e7;e8;e9;e10;e11;e12;e13;e14;e15];;
+let allEs = [e1;e2;e3;e4;e5;e6;e7;e8;e9;e10;e11;e12;e13;e14;e15;e16;e17];;
 
 let rec runAll e = match e with
   | (hd::tl) ->
